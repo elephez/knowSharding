954ea281f93dbfbfaa755e074da2531a148d387b
zhuo <joelee_mail@163.com>
2021-01-22 08:43:51 +0800

    [init] project init

---
+++ src/main/java/com/elephez/knowSharding/KnowShardingApplication.java
@@ -0,0 +1,16 @@
+package com.elephez.knowSharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuo
+ */
+@SpringBootApplication
+public class KnowShardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowShardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elephez/knowSharding/KnowShardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elephez.knowSharding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowShardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
